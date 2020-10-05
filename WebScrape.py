7264911390e9b17240d15f604731cf73bfb8532c
Tony Lee <tloeney1785@gmail.com>
2020-10-05 16:48:23 -0700

    Update WebScrape.py
    can loop over pages and read urls in the page /doctors/ BUT having issues with content disappearing when going directly ?page=2 and so on

--- WebScrape.py
+++ WebScrape.py
@@ -20,28 +20,11 @@ def test(n):
         test(no)
 
 test(no)
-#Loop to go over all pages
-# pages = np.arange(1, 3, 1)
 data=[]
-
-for page in pages:
-    
-    page= "https://www.leafly.com/finder/doctors/ontario-or?page=" + str(page) 
-    driver = webdriver.Chrome()
-    driver.get(page)  
-    sleep(randint(2,10))
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    my_table = soup.find_all(class_=['description', 'price-label body-3','price title-5',\
-                                    'score orange big'])
-
-    for tag in my_table:
-        data.append(tag.get_text())
 
 ##############################
 #First loop: getting the URLs 
 ##############################
-
-pages = np.arange(1, 3, 1)
 
 # for page in pages:
 page="https://www.leafly.com/finder/doctors/salem-or" 
@@ -56,9 +39,8 @@ urls = [item.get("href") for item in soup.find_all("a")]
 urls_final = list(dict.fromkeys(urls))
 urls_final = list(filter(None, urls_final)) 
 
-#Remove if not starting with pwa, remove if ending with display=reviews
+#Remove if not starting with /doctors/
 url_final = [x for x in urls_final if x.startswith('/doctors/')]
-# url_final = [x for x in url_final if not x.endswith('display=reviews')]
    
 string = 'https://www.leafly.com'
 final_list=[string + s for s in url_final]
