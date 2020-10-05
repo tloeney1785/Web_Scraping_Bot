fe34c3267fd659757f6a73235656f08d93c40e26
Tony Lee <tloeney1785@gmail.com>
2020-10-05 15:37:02 -0700

    installed chromedriver

--- WebScrape.py
+++ WebScrape.py
@@ -26,3 +26,30 @@ for page in pages:
 
     for tag in my_table:
         data.append(tag.get_text())
+
+##############################
+#First loop: getting the URLs 
+##############################
+
+pages = np.arange(1, 3, 1)
+url_collected=[]
+
+for page in pages:
+    page="https://www.leafly.com/finder/doctors/salem-or" 
+    driver = webdriver.Chrome()
+    driver.get(page)  
+    sleep(randint(5,15))
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    urls = [item.get("href") for item in soup.find_all("a")]
+    
+    
+#Remove duplicates and none values
+urls_final = list(dict.fromkeys(url_collected))
+urls_final = list(filter(None, urls_final)) 
+
+#Remove if not starting with pwa, remove if ending with display=reviews
+url_final = [x for x in urls_final if x.startswith('/pwa/')]
+url_final = [x for x in url_final if not x.endswith('display=reviews')]
+   
+string = 'https://www.leafly.com/finder/doctors/salem-or'
+final_list=[string + s for s in url_final]
