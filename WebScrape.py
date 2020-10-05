11d660aa975893e45c94ef6bf8b8437d1b31040c
Tony Lee <tloeney1785@gmail.com>
2020-10-05 15:52:47 -0700

    Update WebScrape.py
    update code to filter for /doctors/

--- WebScrape.py
+++ WebScrape.py
@@ -13,19 +13,19 @@ from selenium import webdriver
 pages = np.arange(1, 3, 1)
 data=[]
 
-for page in pages:
+# for page in pages:
     
-    page="https://www.hostelworld.com/s?q=Barcelona,%20Catalonia,%20Spain&country=Spain&city=\
-            Barcelona&type=city&id=83&from=2020-07-03&to=2020-07-05&guests=1&page=" + str(page) 
-    driver = webdriver.Chrome()
-    driver.get(page)  
-    sleep(randint(2,10))
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    my_table = soup.find_all(class_=['description', 'price-label body-3','price title-5',\
-                                    'score orange big'])
+#     page="https://www.hostelworld.com/s?q=Barcelona,%20Catalonia,%20Spain&country=Spain&city=\
+#             Barcelona&type=city&id=83&from=2020-07-03&to=2020-07-05&guests=1&page=" + str(page) 
+#     driver = webdriver.Chrome()
+#     driver.get(page)  
+#     sleep(randint(2,10))
+#     soup = BeautifulSoup(driver.page_source, 'html.parser')
+#     my_table = soup.find_all(class_=['description', 'price-label body-3','price title-5',\
+#                                     'score orange big'])
 
-    for tag in my_table:
-        data.append(tag.get_text())
+#     for tag in my_table:
+#         data.append(tag.get_text())
 
 ##############################
 #First loop: getting the URLs 
@@ -34,22 +34,23 @@ for page in pages:
 pages = np.arange(1, 3, 1)
 url_collected=[]
 
-for page in pages:
-    page="https://www.leafly.com/finder/doctors/salem-or" 
-    driver = webdriver.Chrome()
-    driver.get(page)  
-    sleep(randint(5,15))
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    urls = [item.get("href") for item in soup.find_all("a")]
-    
+# for page in pages:
+page="https://www.leafly.com/finder/doctors/salem-or" 
+driver = webdriver.Chrome()
+driver.get(page)  
+sleep(randint(5,15))
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+urls = [item.get("href") for item in soup.find_all("a")]
+
     
 #Remove duplicates and none values
 urls_final = list(dict.fromkeys(url_collected))
 urls_final = list(filter(None, urls_final)) 
 
 #Remove if not starting with pwa, remove if ending with display=reviews
-url_final = [x for x in urls_final if x.startswith('/pwa/')]
-url_final = [x for x in url_final if not x.endswith('display=reviews')]
+url_final = [x for x in urls_final if x.startswith('/doctors/')]
+# url_final = [x for x in url_final if not x.endswith('display=reviews')]
    
-string = 'https://www.leafly.com/finder/doctors/salem-or'
+string = 'https://www.leafly.com/'
 final_list=[string + s for s in url_final]
+print(final_list)
